18d2c54ba7fadb20c4951336a75f93d1039811cd
ihawp <ihawpebay@gmail.com>
2025-04-19 10:47:33 -0600

    index.php as router

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 
-include_once 'backend/isLogged.php';
+// use as SPA*
+
+require 'vendor/autoload.php';
+
+$location = $_SERVER['REQUEST_URI'];
+
+const PREPEND = '/pipe-q/';
+
+switch ($location) {
+    case PREPEND . 'game':
+        include 'game.php';
+        break;
+
+}
+
+
+
+
+
+// include_once 'backend/isLogged.php';
 
 include_once 'html/head.html';
 include_once 'html/NL-header.html';
